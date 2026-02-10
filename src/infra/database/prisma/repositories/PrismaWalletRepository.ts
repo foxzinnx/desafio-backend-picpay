0d3ab711c0248b6c43c4062fc339e9f602082b85
foxz1n <bryangomes16624@gmail.com>
2026-02-10 17:49:51 -0300

    feat: prisma wallet repository

---
+++ src/infra/database/prisma/repositories/PrismaWalletRepository.ts
@@ -0,0 +1,52 @@
+import type { IWalletRepository } from "@/application/repositories/IWalletRepository.js";
+import type { UserType } from "@/domain/entities/User.js";
+import { Wallet } from "@/domain/entities/Wallet.js";
+import { PrismaClient } from '@/generated/prisma/client.js'
+
+export class PrismaWalletRepository implements IWalletRepository {
+    constructor( private prisma: PrismaClient){}
+    
+    async findById(id: string): Promise<Wallet | null>{
+        const prismaWallet = await this.prisma.wallet.findFirst({
+            where: {
+                OR: [
+                    { id },
+                    { userId: id }
+                ]
+            },
+            include: { user: true },
+        });
+
+        if(!prismaWallet) return null;
+
+        return new Wallet(
+            prismaWallet.id,
+            prismaWallet.userId,
+            Number(prismaWallet.balance),
+            prismaWallet.user.type as UserType
+        )
+    }
+
+    async performTransfer(payer: Wallet, payee: Wallet, amount: number): Promise<void> {
+        await this.prisma.$transaction([
+            this.prisma.wallet.update({
+                where: { id: payer.id },
+                data: { balance: { decrement: amount } },
+            }),
+            
+            this.prisma.wallet.update({
+                where: { id: payee.id },
+                data: { balance: { increment: amount } },
+            }),
+
+            this.prisma.transaction.create({
+                data: {
+                    payerId: payer.id,
+                    payeeId: payee.id,
+                    value: amount
+                }
+            })
+        ])
+    }
+
+}
