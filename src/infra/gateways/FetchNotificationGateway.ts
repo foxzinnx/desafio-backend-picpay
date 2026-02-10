3b2f8fd05c309f5cf1e3f3117eb789d880bda517
foxz1n <bryangomes16624@gmail.com>
2026-02-10 19:13:21 -0300

    feat: add FetchNotificationGateway

---
+++ src/infra/gateways/FetchNotificationGateway.ts
@@ -0,0 +1,16 @@
+import type { INotificationGateway } from "@/application/repositories/INotificationGateway.js";
+
+export class FetchAuthorizerGateway implements INotificationGateway {
+    async send(userId: string, message: string): Promise<void> {
+        const response = await fetch('https://util.devi.tools/api/v1/notify', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ userId, message }),
+        });
+
+        if(!response.ok){
+            throw new Error('Notification service not working.');
+        }
+    }
+
+}
